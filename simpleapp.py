3780afca32d39ac495140e2f122cabf862070bbf
brianmickel <brianemickel@berkeley.edu>
2017-11-16 03:11:43 -0800

    Update simpleapp.py

--- simpleapp.py
+++ simpleapp.py
@@ -9,7 +9,7 @@ start = int(round(time.time()))
 
 @app.route("/")
 def hello_world():
-    return "Hello world from Brian1!"
+    return "Hello world from Bria21!"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
